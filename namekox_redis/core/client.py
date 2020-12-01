959776e95ed78e85c9dabb7b0149de03d04fa480
李满满 <limanman@corp.netease.com>
2020-12-01 15:21:30 +0800

    :underage:

--- namekox_redis/core/client.py
+++ namekox_redis/core/client.py
@@ -16,7 +16,7 @@ class RedisClient(StrictRedis):
         super(RedisClient, self).__init__(*args, **kwargs)
 
     def publish(self, channel, message):
-        headers = gen_message_headers(self.context.data)
+        headers = gen_message_headers(self.context.data) if self.context else {}
         resdata = {'headers': headers, 'message': message}
         return super(RedisClient, self).publish(channel, json.dumps(resdata))
 
