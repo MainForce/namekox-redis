395708a658083e3e77f538161d2f2f2b3e249f3f
李满满 <limanman@corp.netease.com>
2021-04-29 13:07:48 +0800

    :underage:

--- namekox_redis/core/dlock.py
+++ namekox_redis/core/dlock.py
@@ -13,15 +13,13 @@ logger = getLogger(__name__)
 
 
 def distributed_lock(conn, func, name, **kwargs):
-    while True:
-        try:
-            with conn.lock(name, **kwargs):
-                msg = '{} acquire `{}` lock({}) succ'.format(func.__name__, name, kwargs)
-                logger.debug(msg)
-                func()
-            msg = '{} release `{}` lock({}) succ'.format(func.__name__, name, kwargs)
+    try:
+        with conn.lock(name, **kwargs):
+            msg = '{} acquire `{}` lock({}) succ'.format(func.__name__, name, kwargs)
             logger.debug(msg)
-            break
-        except LockError:
-            msg = '{} waiting `{}` lock released'.format(func.__name__, name)
-            logger.debug(msg)
+            func()
+        msg = '{} release `{}` lock({}) succ'.format(func.__name__, name, kwargs)
+        logger.debug(msg)
+    except LockError:
+        msg = '{} waiting `{}` lock released'.format(func.__name__, name)
+        logger.debug(msg)
